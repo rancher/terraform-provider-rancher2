345274f9d266f92f66c41666719dd6c4d03d61d5
rawmind0 <rawmind@gmail.com>
2019-10-07 19:48:25 +0200

    Added rancher2_alert_group and rancher2_alert_rule datasource and resource. Go and test files

---
+++ rancher2/structure_recipient.go
@@ -0,0 +1,52 @@
+package rancher2
+
+import (
+	managementClient "github.com/rancher/types/client/management/v3"
+)
+
+// Flatteners
+
+func flattenRecipients(p []managementClient.Recipient) []interface{} {
+	if len(p) == 0 {
+		return []interface{}{}
+	}
+
+	out := make([]interface{}, len(p))
+	for i, in := range p {
+		obj := make(map[string]interface{})
+
+		obj["notifier_id"] = in.NotifierID
+
+		if len(in.NotifierType) > 0 {
+			obj["notifier_type"] = in.NotifierType
+		}
+
+		if len(in.Recipient) > 0 {
+			obj["recipient"] = in.Recipient
+		}
+
+		out[i] = obj
+	}
+
+	return out
+}
+
+// Expanders
+
+func expandRecipients(p []interface{}) []managementClient.Recipient {
+	if len(p) == 0 || p[0] == nil {
+		return []managementClient.Recipient{}
+	}
+
+	obj := make([]managementClient.Recipient, len(p))
+
+	for i := range p {
+		in := p[i].(map[string]interface{})
+
+		obj[i].NotifierID = in["notifier_id"].(string)
+		obj[i].NotifierType = in["notifier_type"].(string)
+		obj[i].Recipient = in["recipient"].(string)
+	}
+
+	return obj
+}
