0229773c7b40a937dd2356c3e3b42dc64441c042
rawmind0 <rawmind@gmail.com>
2019-10-31 21:27:06 +0100

    Feat: new rancher2_token reosurce. Go and test files

---
+++ rancher2/schema_token.go
@@ -0,0 +1,94 @@
+package rancher2
+
+import (
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+)
+
+const (
+	tokenDefaultSessionDesc = "Terraform token temp token"
+	tokenDefaultTTL         = "60000"
+)
+
+//Schemas
+
+func tokenFields() map[string]*schema.Schema {
+	s := map[string]*schema.Schema{
+		"access_key": {
+			Type:        schema.TypeString,
+			Computed:    true,
+			Description: "Token access key",
+		},
+		"cluster_id": {
+			Type:        schema.TypeString,
+			Optional:    true,
+			ForceNew:    true,
+			Description: "Cluster ID for scoped token",
+		},
+		"description": {
+			Type:        schema.TypeString,
+			Optional:    true,
+			ForceNew:    true,
+			Description: "Token description",
+		},
+		"enabled": {
+			Type:        schema.TypeBool,
+			Computed:    true,
+			Description: "Token enabled",
+		},
+		"expired": {
+			Type:        schema.TypeBool,
+			Computed:    true,
+			Description: "Token expired",
+		},
+		"name": {
+			Type:        schema.TypeString,
+			Computed:    true,
+			Description: "Token name",
+		},
+		"renew": {
+			Type:        schema.TypeBool,
+			Optional:    true,
+			ForceNew:    true,
+			Default:     true,
+			Description: "Renew expired or disabled token",
+		},
+		"secret_key": {
+			Type:        schema.TypeString,
+			Computed:    true,
+			Sensitive:   true,
+			Description: "Token secret key",
+		},
+		"token": {
+			Type:        schema.TypeString,
+			Computed:    true,
+			Sensitive:   true,
+			Description: "Token value",
+		},
+		"ttl": {
+			Type:        schema.TypeInt,
+			Optional:    true,
+			ForceNew:    true,
+			Default:     0,
+			Description: "Token time to live in seconds",
+		},
+		"user_id": {
+			Type:        schema.TypeString,
+			Computed:    true,
+			Description: "Token user ID",
+		},
+		"annotations": &schema.Schema{
+			Type:        schema.TypeMap,
+			Optional:    true,
+			Computed:    true,
+			Description: "Annotations of the token",
+		},
+		"labels": &schema.Schema{
+			Type:        schema.TypeMap,
+			Optional:    true,
+			Computed:    true,
+			Description: "Labels of the token",
+		},
+	}
+
+	return s
+}
