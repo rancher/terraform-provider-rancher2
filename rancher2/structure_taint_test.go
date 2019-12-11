524ec4721aeb1129a34e47dada9549d4148cd7c0
rawmind0 <rawmind@gmail.com>
2019-12-11 11:15:05 +0100

    Added delete_not_ready_after_secs and node_taints arguments to node_pool resource. Go and test files

---
+++ rancher2/structure_taint_test.go
@@ -0,0 +1,74 @@
+package rancher2
+
+import (
+	"reflect"
+	"testing"
+
+	managementClient "github.com/rancher/types/client/management/v3"
+)
+
+var (
+	testTaintsConf      []managementClient.Taint
+	testTaintsInterface []interface{}
+)
+
+func init() {
+	testTaintsConf = []managementClient.Taint{
+		{
+			Key:       "key",
+			Value:     "value",
+			Effect:    "recipient",
+			TimeAdded: "time_added",
+		},
+	}
+	testTaintsInterface = []interface{}{
+		map[string]interface{}{
+			"key":        "key",
+			"value":      "value",
+			"effect":     "recipient",
+			"time_added": "time_added",
+		},
+	}
+}
+
+func TestFlattenTaints(t *testing.T) {
+
+	cases := []struct {
+		Input          []managementClient.Taint
+		ExpectedOutput []interface{}
+	}{
+		{
+			testTaintsConf,
+			testTaintsInterface,
+		},
+	}
+
+	for _, tc := range cases {
+		output := flattenTaints(tc.Input)
+		if !reflect.DeepEqual(output, tc.ExpectedOutput) {
+			t.Fatalf("Unexpected output from flattener.\nExpected: %#v\nGiven:    %#v",
+				tc.ExpectedOutput, output)
+		}
+	}
+}
+
+func TestExpandTaints(t *testing.T) {
+
+	cases := []struct {
+		Input          []interface{}
+		ExpectedOutput []managementClient.Taint
+	}{
+		{
+			testTaintsInterface,
+			testTaintsConf,
+		},
+	}
+
+	for _, tc := range cases {
+		output := expandTaints(tc.Input)
+		if !reflect.DeepEqual(output, tc.ExpectedOutput) {
+			t.Fatalf("Unexpected output from expander.\nExpected: %#v\nGiven:    %#v",
+				tc.ExpectedOutput, output)
+		}
+	}
+}
