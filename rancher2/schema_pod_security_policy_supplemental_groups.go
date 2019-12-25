0e5c5ee25eca7c372b1e73dc58fa1fd0f9e2343c
Alex Seymour <alexander.seymour@rancher.com>
2019-12-25 22:40:02 +0000

    Fix singular range to plural ranges

--- rancher2/schema_pod_security_policy_supplemental_groups.go
+++ rancher2/schema_pod_security_policy_supplemental_groups.go
@@ -23,7 +23,7 @@ var (
 
 func podSecurityPolicySupplementalGroupsFields() map[string]*schema.Schema {
     s := map[string]*schema.Schema{
-        "range": {
+        "ranges": {
             Type:        schema.TypeList,
             Description: "ranges are the allowed ranges of supplemental groups.  If you would like to force a single supplemental group then supply a single range with the same start and end. Required for MustRunAs.",
             Optional:    true,
