794b1ad140c2822c6d8886230a94c031e944ac68
Alex Seymour <axeal@users.noreply.github.com>
2019-12-26 11:09:40 +0000

    Fix optional/required setting for SELinuxOptions

--- rancher2/schema_pod_security_policy_se_linux_options.go
+++ rancher2/schema_pod_security_policy_se_linux_options.go
@@ -11,22 +11,22 @@ func podSecurityPolicySELinuxOptionsFields() map[string]*schema.Schema {
 		"level": {
 			Type:        schema.TypeString,
 			Description: "Level is SELinux level label that applies to the container.",
-			Required:    true,
+			Optional:    true,
 		},
 		"role": {
 			Type:        schema.TypeString,
 			Description: "Role is a SELinux role label that applies to the container.",
-			Required:    true,
+			Optional:    true,
 		},
 		"type": {
 			Type:        schema.TypeString,
 			Description: "Type is a SELinux type label that applies to the container.",
-			Required:    true,
+			Optional:    true,
 		},
 		"user": {
 			Type:        schema.TypeString,
 			Description: "User is a SELinux user label that applies to the container.",
-			Required:    true,
+			Optional:    true,
 		},
 	}
 
