a90b8847e484598b4f895f83b2f6b867123644ff
rawmind0 <rawmind@gmail.com>
2020-03-30 03:15:37 +0200

    Sync rancher2_pod_security_policy_template resource delete with rancher API

--- rancher2/resource_rancher2_pod_security_policy_template.go
+++ rancher2/resource_rancher2_pod_security_policy_template.go
@@ -2,9 +2,12 @@ package rancher2
 
 import (
 	"fmt"
-	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"log"
 	"time"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	managementClient "github.com/rancher/types/client/management/v3"
 )
 
 func resourceRancher2PodSecurityPolicyTemplate() *schema.Resource {
@@ -41,11 +44,6 @@ func resourceRancher2PodSecurityPolicyTemplateCreate(d *schema.ResourceData, met
 		return err
 	}
 
-	err = flattenPodSecurityPolicyTemplate(d, newPodSecurityPolicyTemplate)
-	if err != nil {
-		return err
-	}
-
 	d.SetId(newPodSecurityPolicyTemplate.ID)
 
 	return resourceRancher2PodSecurityPolicyTemplateRead(d, meta)
@@ -68,12 +66,7 @@ func resourceRancher2PodSecurityPolicyTemplateRead(d *schema.ResourceData, meta 
 		return err
 	}
 
-	err = flattenPodSecurityPolicyTemplate(d, pspt)
-	if err != nil {
-		return err
-	}
-
-	return nil
+	return flattenPodSecurityPolicyTemplate(d, pspt)
 }
 
 func resourceRancher2PodSecurityPolicyTemplateUpdate(d *schema.ResourceData, meta interface{}) error {
@@ -99,16 +92,17 @@ func resourceRancher2PodSecurityPolicyTemplateUpdate(d *schema.ResourceData, met
 }
 
 func resourceRancher2PodSecurityPolicyTemplateDelete(d *schema.ResourceData, meta interface{}) error {
-	log.Printf("[INFO] Deleting PodSecurityPolicyTemplate with ID %s", d.Id())
+	id := d.Id()
+	log.Printf("[INFO] Deleting PodSecurityPolicyTemplate with ID %s", id)
 	client, err := meta.(*Config).ManagementClient()
 	if err != nil {
 		return err
 	}
 
-	pspt, err := client.PodSecurityPolicyTemplate.ByID(d.Id())
+	pspt, err := client.PodSecurityPolicyTemplate.ByID(id)
 	if err != nil {
 		if IsNotFound(err) {
-			log.Printf("[INFO] PodSecurityPolicyTemplate with ID %s not found.", d.Id())
+			log.Printf("[INFO] PodSecurityPolicyTemplate with ID %s not found.", id)
 			d.SetId("")
 			return nil
 		}
@@ -120,6 +114,36 @@ func resourceRancher2PodSecurityPolicyTemplateDelete(d *schema.ResourceData, met
 		return fmt.Errorf("Error removing PodSecurityPolicyTemplate: %s", err)
 	}
 
+	stateConf := &resource.StateChangeConf{
+		Pending:    []string{"active"},
+		Target:     []string{"removed"},
+		Refresh:    podSecurityPolicyTemplateStateRefreshFunc(client, id),
+		Timeout:    d.Timeout(schema.TimeoutDelete),
+		Delay:      1 * time.Second,
+		MinTimeout: 3 * time.Second,
+	}
+
+	_, waitErr := stateConf.WaitForState()
+	if waitErr != nil {
+		return fmt.Errorf(
+			"[ERROR] waiting for PodSecurityPolicyTemplate (%s) to be removed: %s", id, waitErr)
+	}
+
 	d.SetId("")
 	return nil
+}
+
+// podSecurityPolicyTemplateStateRefreshFunc returns a resource.StateRefreshFunc, used to watch a Rancher PodSecurityPolicyTemplate
+func podSecurityPolicyTemplateStateRefreshFunc(client *managementClient.Client, pspID string) resource.StateRefreshFunc {
+	return func() (interface{}, string, error) {
+		obj, err := client.PodSecurityPolicyTemplate.ByID(pspID)
+		if err != nil {
+			if IsNotFound(err) {
+				return obj, "removed", nil
+			}
+			return nil, "", err
+		}
+
+		return obj, "active", nil
+	}
 }
