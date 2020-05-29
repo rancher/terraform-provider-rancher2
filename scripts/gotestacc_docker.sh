9d71058570180f4b4ea61f4870accbb9b4d4aad5
rawmind0 <rawmind@gmail.com>
2020-05-29 13:15:09 +0200

    Updated golang to 1.13

--- scripts/gotestacc_docker.sh
+++ scripts/gotestacc_docker.sh
@@ -22,4 +22,5 @@ ${DOCKER_BIN} build -t ${BUILDER_TAG} -f $(dirname $0)/Dockerfile.builder .
 ${DOCKER_BIN} run -i --rm \
   -v /var/run/docker.sock:/var/run/docker.sock \
   -v $PWD:/go/src/github.com/terraform-providers/terraform-provider-rancher2 \
+  -e EXPOSE_HOST_PORTS=${EXPOSE_HOST_PORTS} \
   ${BUILDER_TAG} make testacc
