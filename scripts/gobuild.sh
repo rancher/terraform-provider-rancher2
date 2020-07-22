c04c40c0110fb984f7436e58ef9e13aa9e79d851
rawmind0 <rawmind@gmail.com>
2020-07-22 15:17:12 +0200

    Added drone pipeline to own the repo

--- scripts/gobuild.sh
+++ scripts/gobuild.sh
@@ -2,29 +2,31 @@
 
 set -e
 
-echo "==> Building code binaries..."
-
 source $(dirname $0)/version.sh
+
+echo "==> Building code binaries version ${VERSION} ..."
 
 declare -A OS_ARCH_ARG
 
-OS_PLATFORM_ARG=(linux windows darwin)
-OS_ARCH_ARG[linux]="amd64 arm"
+OS_PLATFORM_ARG=(linux windows darwin freebsd openbsd)
+OS_ARCH_ARG[linux]="amd64 arm arm64"
 OS_ARCH_ARG[windows]="386 amd64"
 OS_ARCH_ARG[darwin]="amd64"
+OS_ARCH_ARG[freebsd]="386 amd64 arm"
+OS_ARCH_ARG[openbsd]="386 amd64"
 
 BIN_NAME="terraform-provider-rancher2"
 BUILD_DIR=$(dirname $0)"/../build/bin"
 
 
-CGO_ENABLED=0 go build -ldflags="-w -s -X main.VERSION=$VERSION -linkmode external -extldflags -static" -o bin/${BIN_NAME}
+CGO_ENABLED=0 go build -ldflags="-w -s -X main.VERSION=$VERSION -extldflags -static" -o bin/${BIN_NAME}
 
 if [ -n "$CROSS" ]; then
     rm -rf ${BUILD_DIR}
     mkdir -p ${BUILD_DIR}
     for OS in ${OS_PLATFORM_ARG[@]}; do
         for ARCH in ${OS_ARCH_ARG[${OS}]}; do
-            OUTPUT_BIN="${BUILD_DIR}/${BIN_NAME}_$OS-$ARCH"
+            OUTPUT_BIN="${BUILD_DIR}/${BIN_NAME}_${OS}_${ARCH}"
             if test "$OS" = "windows"; then
                 OUTPUT_BIN="${OUTPUT_BIN}.exe"
             fi
