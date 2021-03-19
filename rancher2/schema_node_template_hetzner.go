e9d3c876828490357c9ef9ccac4de904b38b77c1
Alexander Zimmermann <alexander.zimmermann96@gmail.com>
2021-03-19 18:25:49 +0100

    Fixed type mismatch in Hetzner Node Template
    networks and volumes need to be parsed to arrays

--- rancher2/schema_node_template_hetzner.go
+++ rancher2/schema_node_template_hetzner.go
@@ -11,14 +11,14 @@ const (
 //Types
 
 type hetznerConfig struct {
-	APIToken          string `json:"apiToken,omitempty" yaml:"apiToken,omitempty"`
-	Image             string `json:"image,omitempty" yaml:"image,omitempty"`
-	ServerLocation    string `json:"serverLocation,omitempty" yaml:"serverLocation,omitempty"`
-	ServerType        string `json:"serverType,omitempty" yaml:"serverType,omitempty"`
-	Networks          string `json:"networks,omitempty" yaml:"networks,omitempty"`
-	UsePrivateNetwork bool   `json:"usePrivateNetworks,omitempty" yaml:"usePrivateNetworks,omitempty"`
-	UserData          string `json:"userData,omitempty" yaml:"userData,omitempty"`
-	Volumes           string `json:"volumes,omitempty" yaml:"volumes,omitempty"`
+	APIToken          string   `json:"apiToken,omitempty" yaml:"apiToken,omitempty"`
+	Image             string   `json:"image,omitempty" yaml:"image,omitempty"`
+	ServerLocation    string   `json:"serverLocation,omitempty" yaml:"serverLocation,omitempty"`
+	ServerType        string   `json:"serverType,omitempty" yaml:"serverType,omitempty"`
+	Networks          []string `json:"networks,omitempty" yaml:"networks,omitempty"`
+	UsePrivateNetwork bool     `json:"usePrivateNetworks,omitempty" yaml:"usePrivateNetworks,omitempty"`
+	UserData          string   `json:"userData,omitempty" yaml:"userData,omitempty"`
+	Volumes           []string `json:"volumes,omitempty" yaml:"volumes,omitempty"`
 }
 
 //Schemas
