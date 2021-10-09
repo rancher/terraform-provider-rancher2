842375887d9a80d148ca9349c7a525883e1b0697
Nauris <nauris.sadovskis@gmail.com>
2021-10-09 09:30:13 +0200

    Add data source for reading principal information

---
+++ rancher2/data_source_rancher2_principal.go
@@ -0,0 +1,72 @@
+package rancher2
+
+import (
+	"fmt"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	managementClient "github.com/rancher/rancher/pkg/client/generated/management/v3"
+)
+
+func dataSourceRancher2Principal() *schema.Resource {
+	return &schema.Resource{
+		Read: dataSourceRancher2PrincipalRead,
+
+		Schema: map[string]*schema.Schema{
+			"email": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"type": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"provider_name": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func dataSourceRancher2PrincipalRead(d *schema.ResourceData, meta interface{}) error {
+	client, err := meta.(*Config).ManagementClient()
+	if err != nil {
+		return err
+	}
+
+	email := d.Get("email").(string)
+	principalType := d.Get("type").(string)
+
+	collection, err := client.Principal.List(nil)
+	if err != nil {
+		return err
+	}
+
+	principals, err := client.Principal.CollectionActionSearch(collection, &managementClient.SearchPrincipalsInput{
+		Name:          email,
+		PrincipalType: principalType,
+	})
+	if err != nil {
+		return err
+	}
+
+	count := len(principals.Data)
+	if count <= 0 {
+		return fmt.Errorf("[ERROR] principal \"%s\" of type \"%s\" not found", email, principalType)
+	}
+
+	return flattenDataSourcePrincipal(d, &principals.Data[0])
+}
+
+func flattenDataSourcePrincipal(d *schema.ResourceData, in *managementClient.Principal) error {
+	if in == nil {
+		return nil
+	}
+
+	d.SetId(in.ID)
+	d.Set("id", in.ID)
+	d.Set("type", in.PrincipalType)
+	d.Set("provider_name", in.Provider)
+
+	return nil
+}
