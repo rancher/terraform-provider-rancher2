c2de940cfd6b5dd574b8b53295cc3633c6afdf5b
Greg <gate@ilive4code.net>
2023-02-02 12:05:10 -0500

    Do not sort mirror endpoints because ordering could matter (#1029)

--- rancher2/structure_cluster_v2_rke_config_registry.go
+++ rancher2/structure_cluster_v2_rke_config_registry.go
@@ -61,7 +61,7 @@ func flattenClusterV2RKEConfigRegistryMirrors(p map[string]rkev1.Mirror) []inter
 		obj["hostname"] = k
 
 		if len(in.Endpoints) > 0 {
-			obj["endpoints"] = toArrayInterfaceSorted(in.Endpoints)
+			obj["endpoints"] = toArrayInterface(in.Endpoints)
 		}
 		if len(in.Rewrites) > 0 {
 			obj["rewrites"] = toMapInterface(in.Rewrites)
@@ -129,7 +129,7 @@ func expandClusterV2RKEConfigRegistryMirrors(p []interface{}) map[string]rkev1.M
 		obj := rkev1.Mirror{}
 
 		if v, ok := in["endpoints"].([]interface{}); ok && len(v) > 0 {
-			obj.Endpoints = toArrayStringSorted(v)
+			obj.Endpoints = toArrayString(v)
 		}
 		if v, ok := in["rewrites"].(map[string]interface{}); ok && len(v) > 0 {
 			obj.Rewrites = toMapString(v)
