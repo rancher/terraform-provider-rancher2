965005bd60ae77bdb10d021b238f19961e2666fd
Anna Blendermann <anna.blendermann@suse.com>
2023-04-05 12:16:05 -0400

    Tweak script

--- setup-provider.sh
+++ setup-provider.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-# QA has a new process to test Terraform using rcs. We will not publish rcs
+# QA has a new process to test Terraform using RCs. We will not publish RCs
 # onto the Terraform registry because Hashicorp could potentially block our
 # testing by being slow to publish.
 
-# Instead, we will test using a downloaded binary from the rc. This script
+# Instead, we will test using a downloaded binary from the RC. This script
 # sets up the correct binary using a defined <provider> <version> to test
 # updates locally.
+
+# ./setup-provider.sh <provider> <version>
 
 # Example
 # ./setup-provider.sh rancher2 v3.0.0-rc1
@@ -40,6 +42,9 @@ OS_PLATFORM=$(uname -sp | tr '[:upper:] ' '[:lower:]_' | sed 's/x86_64/amd64/' |
 DIR=~/.terraform.d/plugins/terraform.local/local/${PROVIDER}/${VERSION_TAG}/${OS_PLATFORM}
 mkdir -p $DIR
 curl -sfL https://github.com/rancher/terraform-provider-${PROVIDER}/releases/download/${VERSION}/terraform-provider-${PROVIDER}_${VERSION_TAG}_${OS_PLATFORM}.zip | gunzip -c - > ${DIR}/terraform-provider-${PROVIDER}
+
+# Mod binary
+chmod +x ${DIR}/terraform-provider-${PROVIDER}
 
 echo -e "Terraform provider ${PROVIDER} ${VERSION} is ready to test!
 Please update the required_providers block in your Terraform config file
