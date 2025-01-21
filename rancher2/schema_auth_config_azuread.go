3f7656ee1d790e54d97d36215c64be3f4e19cad5
Kevin McDermott <kevin.mcdermott@suse.com>
2025-01-21 17:11:36 +0000

    Add support for group_membership_filter in azuread (#1458)
    This adds support for configuring the group_membership_filter for Azure AuthConfig.
    
    Signed-off-by: Kevin McDermott <kevin.mcdermott@suse.com>

--- rancher2/schema_auth_config_azuread.go
+++ rancher2/schema_auth_config_azuread.go
@@ -45,6 +45,10 @@ func authConfigAzureADFields() map[string]*schema.Schema {
 			Type:     schema.TypeString,
 			Required: true,
 		},
+		"group_membership_filter": {
+			Type:     schema.TypeString,
+			Optional: true,
+		},
 	}
 
 	for k, v := range authConfigFields() {
