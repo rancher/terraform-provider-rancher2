4171f24c21716fc21e3d57bb71309e44ff837ddc
matttrach <matt.trachier@suse.com>
2025-07-09 19:17:03 -0500

    fix: formatting
    Signed-off-by: matttrach <matt.trachier@suse.com>

--- examples/state_import/outputs.tf
+++ examples/state_import/outputs.tf
@@ -6,22 +6,22 @@ output "kubeconfig" {
   sensitive   = true
 }
 output "address" {
-  value = module.rancher.address
+  value       = module.rancher.address
   description = <<-EOT
     The url for the rancher cluster.
   EOT
 }
 output "admin_password" {
-  value     = module.rancher.admin_password
+  value       = module.rancher.admin_password
   description = <<-EOT
     The admin password for the rancher cluster.
   EOT
-  sensitive = true
+  sensitive   = true
 }
 output "downstream_kubeconfig" {
-  value     = data.rancher2_cluster.downstream_cluster.kube_config
+  value       = data.rancher2_cluster.downstream_cluster.kube_config
   description = <<-EOT
     The kubeconfig for the downstream cluster.
   EOT
-  sensitive = true
+  sensitive   = true
 }
