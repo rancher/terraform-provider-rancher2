e8a3de02253548394437a8f3b5f146ff4dc4cc60
Matt Trachier <matt.trachier@suse.com>
2025-07-15 15:00:12 -0500

    fix: Clean up test suite (#1587)
    Signed-off-by: matttrach <matt.trachier@suse.com>

---
+++ examples/deploy_downstream_splitrole/outputs.tf
@@ -0,0 +1,18 @@
+output "kubeconfig" {
+  value       = module.rancher.kubeconfig
+  description = <<-EOT
+    The kubeconfig for the server.
+  EOT
+  sensitive   = true
+}
+output "address" {
+  value = module.rancher.address
+}
+output "admin_password" {
+  value     = module.rancher.admin_password
+  sensitive = true
+}
+output "downstream_kubeconfig" {
+  value     = data.rancher2_cluster.downstream.kube_config
+  sensitive = true
+}
