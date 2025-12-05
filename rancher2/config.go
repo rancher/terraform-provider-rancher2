4b109a69ff77ffb788e9ba7f8a723a063dbb98e7
Sergei Starostin <67504894+s-starostin@users.noreply.github.com>
2025-12-05 07:48:51 +0300

    fix: Correct genericoidc provider implementation and tests (#1691)
    (cherry picked from commit 0d7b485f4f5289a9c7f098e618b685731fd3985d)

--- rancher2/config.go
+++ rancher2/config.go
@@ -1272,6 +1272,8 @@ func getAuthConfigObject(kind string) (interface{}, error) {
 		return &managementClient.KeyCloakConfig{}, nil
 	case managementClient.GenericOIDCConfigType:
 		return &managementClient.GenericOIDCConfig{}, nil
+	case managementClient.OIDCConfigType:
+		return &managementClient.OIDCConfig{}, nil
 	case managementClient.OKTAConfigType:
 		return &managementClient.OKTAConfig{}, nil
 	case managementClient.OpenLdapConfigType:
