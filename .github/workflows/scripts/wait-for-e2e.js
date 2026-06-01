5111ba2c139e4858182c6baab37e666bfe366a28
matttrach <matt.trachier@suse.com>
2026-06-01 17:50:47 -0500

    fix: validate number with good error message
    Signed-off-by: matttrach <matt.trachier@suse.com>

--- .github/workflows/scripts/wait-for-e2e.js
+++ .github/workflows/scripts/wait-for-e2e.js
@@ -1,6 +1,11 @@
 export default async ({ github, context, core, process }) => {
   try {
-    const prNumber = parseInt(process.env.PR_NUMBER, 10);
+    const prNumberRaw = process.env.PR_NUMBER;
+    const prNumber = Number.parseInt(prNumberRaw, 10);
+    if (!Number.isFinite(prNumber)) {
+      core.setFailed(`Invalid PR_NUMBER: ${prNumberRaw}`);
+      return;
+    }
     await github.rest.issues.createComment({
       issue_number: prNumber,
       owner: context.repo.owner,
